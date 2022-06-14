fc4d8c49e187ab6141957fc59240af429847d6d3
Dmitry Gusev <dmtrxiii@gmail.com>
2022-06-15 01:24:10 +0300

    - variables renaming - task 4 released

--- app/src/main/java/com/ineedyourcode/customstopwatch/ui/stopwatch/StopwatchFragmentViewModel.kt
+++ app/src/main/java/com/ineedyourcode/customstopwatch/ui/stopwatch/StopwatchFragmentViewModel.kt
@@ -13,28 +13,29 @@ import kotlinx.coroutines.launch
 private const val ARROW_TICKS_COUNT = 600
 private const val ARROW_ROTATION_ANGLE = 360f / ARROW_TICKS_COUNT
 private const val DEFAULT_PREVIOUS_TIME = '0'
+private const val TARGET_CHAR_INDEX = 6
 
 class StopwatchFragmentViewModel(
     private val stopwatchListOrchestrator: StopwatchListOrchestrator,
 ) : ViewModel() {
 
-    private val stopwatchTimeOne: MutableLiveData<String> = MutableLiveData()
-    private val stopwatchTimeTwo: MutableLiveData<String> = MutableLiveData()
-    private val stopwatchArrowOne: MutableLiveData<Float> = MutableLiveData()
-    private val stopwatchArrowTwo: MutableLiveData<Float> = MutableLiveData()
+    private val stopwatchOneTime: MutableLiveData<String> = MutableLiveData()
+    private val stopwatchTwoTime: MutableLiveData<String> = MutableLiveData()
+    private val stopwatchOneArrow: MutableLiveData<Float> = MutableLiveData()
+    private val stopwatchTwoArrow: MutableLiveData<Float> = MutableLiveData()
 
     fun getStopwatchTimeTwo(): LiveData<String> {
         CoroutineScope(Dispatchers.IO + SupervisorJob()).launch {
             var previousTimeInSeconds = DEFAULT_PREVIOUS_TIME
             stopwatchListOrchestrator.tickerTwo.collect {
                 if (it.toCharArray()[6] != previousTimeInSeconds) {
-                    stopwatchArrowTwo.postValue(ARROW_ROTATION_ANGLE)
-                    previousTimeInSeconds = it.toCharArray()[6]
+                    stopwatchTwoArrow.postValue(ARROW_ROTATION_ANGLE)
+                    previousTimeInSeconds = it.toCharArray()[TARGET_CHAR_INDEX]
                 }
-                stopwatchTimeTwo.postValue(it)
+                stopwatchTwoTime.postValue(it)
             }
         }
-        return stopwatchTimeTwo
+        return stopwatchTwoTime
     }
 
     fun getStopwatchTimeOne(): LiveData<String> {
@@ -42,17 +43,17 @@ class StopwatchFragmentViewModel(
             var previousTimeInSeconds = DEFAULT_PREVIOUS_TIME
             stopwatchListOrchestrator.tickerOne.collect {
                 if (it.toCharArray()[6] != previousTimeInSeconds) {
-                    stopwatchArrowOne.postValue(ARROW_ROTATION_ANGLE)
-                    previousTimeInSeconds = it.toCharArray()[6]
+                    stopwatchOneArrow.postValue(ARROW_ROTATION_ANGLE)
+                    previousTimeInSeconds = it.toCharArray()[TARGET_CHAR_INDEX]
                 }
-                stopwatchTimeOne.postValue(it)
+                stopwatchOneTime.postValue(it)
             }
         }
-        return stopwatchTimeOne
+        return stopwatchOneTime
     }
 
-    fun getStopwatchArrowOne(): LiveData<Float> = stopwatchArrowOne
-    fun getStopwatchArrowTwo(): LiveData<Float> = stopwatchArrowTwo
+    fun getStopwatchArrowOne(): LiveData<Float> = stopwatchOneArrow
+    fun getStopwatchArrowTwo(): LiveData<Float> = stopwatchTwoArrow
 
     fun start(stopwatchNumber: StopwatchNumber) {
         stopwatchListOrchestrator.start(stopwatchNumber)
